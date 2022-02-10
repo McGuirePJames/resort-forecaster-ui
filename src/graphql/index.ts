b11009fadf3ec8ba24b194079b43585f13b6338b
James McGuire <mcguirepjames@gmail.com>
2022-02-10 15:10:46 -0700

    Change graphql client url

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 import { getSdk } from '../generated/graphql';
 
-export const graphQLClient = new GraphQLClient('https://localhost:8080/graphql');
+export const graphQLClient = new GraphQLClient('https://ava-map-api-app.azurewebsites.net/graphql');
 
 export const graph = getSdk(graphQLClient);
